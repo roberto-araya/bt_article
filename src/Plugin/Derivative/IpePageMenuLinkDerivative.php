2b12b817a109ba60056e3d0e010494a1dc980f3f
Roberto Araya <roberto.araya@btester.cl>
2018-01-16 00:27:29 -0300

    Issue #2936442 by sarguna raj M, Sivaprasad C, roberto_araya: Fix a lot of code standard details

--- src/Plugin/Derivative/IpePageMenuLinkDerivative.php
+++ src/Plugin/Derivative/IpePageMenuLinkDerivative.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * @file
- */
 
 namespace Drupal\bt_article\Plugin\Derivative;
 
@@ -13,29 +10,30 @@ use Drupal\page_manager\Entity\Page;
  */
 class IpePageMenuLinkDerivative extends DeriverBase {
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getDerivativeDefinitions($base_plugin_definition) {
-        $links = array();
-        $pages = [
-            'articles' => 'All published articles'
-        ];
-        foreach ($pages as $page_name => $description) {
-            $w = 3;
-            $page = Page::load('ipe_' . $page_name);
-            $route_name = 'page_manager.page_view_ipe_' . $page_name . '_ipe_' . $page_name . '-panels_variant-0';
-            if ($page->status()) {
-                $links['main_' . $page->id()] = [
-                        'title' => $page->label(),
-                        'description' => $description,
-                        'menu_name' => 'main',
-                        'weight' => $w,
-                        'route_name' => $route_name,
-                    ] + $base_plugin_definition;
-            }
-            $w++;
-        }
-        return $links;
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    $links = [];
+    $pages = [
+      'articles' => 'All published articles',
+    ];
+    foreach ($pages as $page_name => $description) {
+      $w = 3;
+      $page = Page::load('ipe_' . $page_name);
+      $route_name = 'page_manager.page_view_ipe_' . $page_name . '_ipe_' . $page_name . '-panels_variant-0';
+      if ($page->status()) {
+        $links['main_' . $page->id()] = [
+          'title' => $page->label(),
+          'description' => $description,
+          'menu_name' => 'main',
+          'weight' => $w,
+          'route_name' => $route_name,
+        ] + $base_plugin_definition;
+      }
+      $w++;
     }
+    return $links;
+  }
+
 }
